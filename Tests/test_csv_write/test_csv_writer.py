b243559d35f2ec49d172b7916fbae99ee9bb3520
Przemek <przemic98@gmail.com>
2020-02-09 23:35:51 +0100

    Improved tests

--- Tests/test_csv_write/test_csv_writer.py
+++ Tests/test_csv_write/test_csv_writer.py
@@ -5,21 +5,28 @@ from movies_db import DBConfig, DataDownload, CSVWriter
 
 
 @pytest.fixture(scope='module')
-def database():
-    """ Setup before tests """
+def movies():
+    """ Setup of the example data for the tests"""
 
-    # Setup
     movies = DataDownload(titles_to_get='titles.txt')
     movies.read_titles()
     results = movies.get_titles_using_api()
 
+    yield results
+
+
+@pytest.fixture(scope='module')
+def database(movies):
+    """ Setup of the database before tests """
+
+    # Setup - creating new database
     db = DBConfig(db_name='movies.db')
     db.db_create()
-    db.insert_data(results)
+    db.insert_data(movies)
 
     yield db
 
-    # Teardown
+    # Teardown - deleting the tested table
     drop_table_statement = "DROP TABLE movies"
     db.c.execute(drop_table_statement)
 
@@ -31,16 +38,21 @@ def test_write_csv(database):
     :return:
     """
 
-    # Fail case
+    # Fail case - not existing parameter
     with pytest.raises(sqlite3.OperationalError) as exec_info:
-        results = database.get_data_sort_by('metascorefajne')
-        CSVWriter.write_csv(title='test_metascore_fail.csv', data=results)
+        results = database.get_data_sort_by('metascorefajne')  # Not existing param metascorefajne
     print(exec_info.value)
 
-    # Multiple results
-    results = database.get_data_sort_by('metascore')
-    CSVWriter.write_csv(title='test_metascore.csv', data=results)
+    # Faile case - empty result
+    with pytest.raises(IndexError) as exec_info:
+        results = database.get_data_by_title('KacWawa')
+        CSVWriter.write_csv(title='test_metascore.csv', data=results)
+    print(exec_info.value)
 
     # Single result
     results = database.get_data_by_title('Kac Wawa')
+    CSVWriter.write_csv(title='test_metascore.csv', data=results)
+
+    # Multiple results
+    results = database.get_data_sort_by('metascore')
     CSVWriter.write_csv(title='test_metascore.csv', data=results)
